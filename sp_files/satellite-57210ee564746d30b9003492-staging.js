e407956369b0391a8351de1c294f8b3b90fedd24
Boroda <dboroda@ukr.net>
2016-08-15 12:55:39 +0300

    Commit 25

--- sp_files/satellite-57210ee564746d30b9003492-staging.js
+++
@@ -1,9 +0,0 @@
-_satellite.pushAsyncScript(function(event, target, $variables){
-  	try {var __scP=(document.location.protocol=="https:")?"https://":"http://";
-
-	var __scS=document.createElement("script");__scS.type="text/javascript";
-
-	__scS.async=true;__scS.src=__scP+"d16fk4ms6rqz1v.cloudfront.net/capture/SPORTCHEK.js";
-
-	document.getElementsByTagName("head")[0].appendChild(__scS);}catch(e){}
-});
